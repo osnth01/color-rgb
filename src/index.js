19aaa72998fffc6e683f3be7f4b25d9e7abd3fe0
osnth01 <osnth01@gmail.com>
2016-08-06 21:03:23 -0400

    setup initial app

--- src/index.js
+++ src/index.js
@@ -1,1 +1,16 @@
-console.log('loaded.');
+import React from 'react'
+import ReactDOM from 'react-dom'
+import colors from './reducers'
+import { createStore } from 'redux'
+import { Provider } from 'react-redux'
+
+const App = () => <div>hi</div>
+
+let store = createStore(colors)
+
+ReactDOM.render(
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  document.getElementById('root')
+)
